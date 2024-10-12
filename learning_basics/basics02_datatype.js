78e46670a11644d1c4c5fe55b4f854cabf703644
Shubham-gupta8 <shubham.work27052004@gmail.com>
2024-10-12 08:55:37 +0000

    datatype

---
+++ learning_basics/basics02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code according to  latest version 
+
+
+// aler(3+3) in browser it will run but not in node kyuki browder ki khud ki file hoti h
+// two documentation are very important MDN AND TC39.ES(ecma)
+
+let name = "shubham"  //string
+let age= 19           // number
+let ismale=  true     //boolean
+
+/*        PRIMITIVE DATATYPE
+ 
+number=> 2^53
+bigint
+stirng=> ""
+boolean=> true/false
+null=> standalone value
+undefined=> not initialized but declared
+symbol=> unique
+
+*/
+console.log(typeof ismale);    //return boolean
+console.log(typeof null);      //object
+console.log(typeof undefined)   //undefined
+
+
+
+
+
